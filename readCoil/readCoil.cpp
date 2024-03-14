1044d56e58a29c4ea95f18794af5a23e93c196c5
dyoun04 <dty003@bucknell.edu>
2024-03-14 18:06:36 -0400

    Update readCoil.cpp

--- readCoil/readCoil.cpp
+++ readCoil/readCoil.cpp
@@ -13,14 +13,12 @@
 #include <iostream>
 #include <sys/time.h>
 #include <cmath>
-#include <highfive/highfive.hpp>
+#include <highfive/H5Easy.hpp>
 using namespace std;
-using namespace HighFive;
 
 // Physical constants
 #define VRef 4.096
 #define sampRate 384
-
 
 // PRU defined constants
 #define averaging 8
@@ -31,7 +29,8 @@ off_t SHARED_MEM = 0x4a310000;
 off_t PRU0_MEM = 0x4a300000;
 off_t PRU1_MEM = 0x4a302000;
 // Memory buffers and allocated space
-#define BUFF_SIZE (24*200)
+const int wordsPerBuf = 200;
+#define BUFF_SIZE (24*wordsPerBuf)
 
 // GPIO pins
 const int pps_pin = 48;
@@ -44,7 +43,11 @@ const string PRU1_PATH = "pru/pru1.out";
 const int time_len = 10;
 
 // SD card path
-const string SD_PATH = "/home/debian/examples/SDcard";
+const string SD_PATH = "/home/debian/examples/SDcard/";
+
+// HDF5 files
+const int bufsToFile = 10;
+const int dataLen = bufsToFile*wordsPerBuf;
 
 // Reads current value of gpio pin
 int readGPIO(int gpio) {
@@ -125,6 +128,12 @@ int main(void) {
     } // create path if doesn't exist
     system(("mount /dev/mmcblk0p1 " + SD_PATH).c_str());
 
+    // HDF5 file
+    H5Easy::File file(SD_PATH + "Site_A.h5", H5Easy::File::Overwrite);
+    vector<vector<double>> h5Data(2, vector<double>(dataLen));
+    int vecIndex = 0;
+    int dSetCount = 0;
+
     // buffer vars
     int bufReady = *pru0Data_base;
     uint32_t buf[BUFF_SIZE / 24];
@@ -135,7 +144,7 @@ int main(void) {
     double times[time_len] = {0};
     double dt;
     timeval tv;
-    double sys_time;
+    double time_base;
     // iterators
     int i; // general use
     int j = 0; // time keeping
@@ -196,8 +205,8 @@ int main(void) {
     while(pps == 1) {pps = readGPIO(pps_pin);}
     while(pps == 0) {pps = readGPIO(pps_pin);}
     gettimeofday(&tv, NULL);
-    sys_time = (double) tv.tv_sec + ((double) tv.tv_usec / 1000000);
-    printf("%.6f\n", round(sys_time));
+    time_base = (double) round((double) tv.tv_sec + ((double) tv.tv_usec / 1000000));
+    printf("%.6f\n", time_base);
     PRU_file.open("/sys/class/remoteproc/remoteproc2/state");
     PRU_file << "start";
     PRU_file.close();
@@ -212,13 +221,19 @@ int main(void) {
             bufReady = *pru0Data_base;
             if(bufReady) {wordPtr = firstBuf_base;}
             else         {wordPtr = secondBuf_base;}
-            FILE* dataFile = fopen("data.txt", "a");
             for(i=0; i<BUFF_SIZE / 24; i++) {
                 buf[i] = (*(wordPtr + 2) << 16) | (*(wordPtr + 1) << 8) | *wordPtr;
                 wordPtr += 3;
-                fprintf(dataFile, "%.15f\n", get_voltage(buf[i]));
+                h5Data[0][vecIndex] = time_base;
+                h5Data[1][vecIndex] = get_voltage(buf[i]);
+                time_base += dt;
+                vecIndex++;
             } // for
-            fclose(dataFile);
+            if(vecIndex >= dataLen) {
+                vecIndex = 0;
+                H5Easy::dump(file, "dSet_" + to_string(dSetCount), h5Data);
+                dSetCount++;
+            } // if
         } // if
 
         // Checks for rising edge of PPS
